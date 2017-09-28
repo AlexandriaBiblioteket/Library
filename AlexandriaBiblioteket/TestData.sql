6e66e325d21e28925035c5fa6d436b345c4e45b8
User <User@KARL.lexicon.edu.local>
2017-09-28 15:40:47 +0200

    TestData added

---
+++ AlexandriaBiblioteket/TestData.sql
@@ -0,0 +1,29 @@
+use alexandria;
+
+
+insert into author values (1, 'Jamie Chan');
+insert into media values (100000, 'Java: Learn Java in One Day and Learn It Well.', '978-1539397830', 0, 1, null);
+
+insert into author values (2, 'Kathy Sierra');
+insert into media values (100001, 'Head First Java, 2nd Edition', '978-0596009205', 0, 2, null);
+
+insert into author values (3, 'Herbert Schildt');
+insert into media values (100002, 'Java: The Complete Reference, Ninth Edition', '978-0071808552', 0, 3, null);
+
+insert into author values (4, 'Steve Tale');
+insert into media values (100003, 'Java: The Ultimate Beginners Guide to Java Programming', '978-1539591214', 0, 4, null);
+insert into media values (100004, 'JavaScript: The Ultimate Beginners Guide', '978-1539984634', 0, 4, null);
+insert into media values (100005, 'SQL: The Ultimate Beginners Guide - Learn SQL Today', '978-1533604569', 0, 4, null);
+
+insert into author values (5, 'John Joseph Stockdale');
+insert into media values (100006, 'Island of Java (Periplus Classics Series)', '978-9628734238', 0, 5, null);
+
+insert into author values (6, 'John MacKinnon');
+insert into media values (100007, 'A Field Guide to the Birds of Borneo, Sumatra, Java, and Bali: The Greater Sunda Islands', '978-0198540342', 0, 6, null);
+
+insert into author values (7, 'Dian Pramesti');
+insert into media values (100008, 'Greater Than a Tourist – Java, Indonesia: 50 Travel Tips from a Local', '978-1521199466', 0, 7, null);
+
+insert into author values (8, 'Jeffrey E. F. Friedl');
+insert into media values (100009, 'Mastering Regular Expressions', '978-0596528126', 0, 8, null);
+
