56e827ede514a170101c73235b9335107d167f45
User <kghultland@gmail.com>
2017-09-29 09:52:19 +0200

    Removed person from Media, and fixed foreign keys. (as well as test data)

--- AlexandriaBiblioteket/Alexandria.sql
+++ AlexandriaBiblioteket/Alexandria.sql
@@ -1,13 +1,13 @@
 /*******************************************************************************
  * Selected metadata objects
  * -------------------------
- * Extracted at 2017-09-28 09:41:20
+ * Extracted at 2017-09-29 09:48:52
  ******************************************************************************/
 
 /*******************************************************************************
  * Tables
  * ------
- * Extracted at 2017-09-28 09:41:20
+ * Extracted at 2017-09-29 09:48:52
  ******************************************************************************/
 
 CREATE TABLE author (
@@ -18,6 +18,10 @@ CREATE TABLE author (
   )
 ) ENGINE=InnoDB ROW_FORMAT=DYNAMIC DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE author COMMENT = '';
+CREATE TABLE hibernate_sequence (
+  next_val BigInt(20)
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE hibernate_sequence COMMENT = '';
 CREATE TABLE loan (
   ID       Integer(11) NOT NULL,
   personID Integer(11),
@@ -36,13 +40,22 @@ CREATE TABLE media (
   `name`   NVarChar(200) COLLATE utf8_general_ci,
   ISBN     NVarChar(32) COLLATE utf8_general_ci,
   status   TinyInt(4),
-  authorID Integer(11),
-  personID Integer(11), 
+  authorID Integer(11), 
   PRIMARY KEY (
       ID
   )
 ) ENGINE=InnoDB ROW_FORMAT=DYNAMIC DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE media COMMENT = '';
+CREATE TABLE member (
+  id           BigInt(20) NOT NULL,
+  email        NVarChar(255) COLLATE utf8_general_ci NOT NULL,
+  `name`       NVarChar(25) COLLATE utf8_general_ci NOT NULL,
+  phone_number NVarChar(12) COLLATE utf8_general_ci NOT NULL, 
+  PRIMARY KEY (
+      id
+  )
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE member COMMENT = '';
 CREATE TABLE person (
   ID     Integer(11) NOT NULL,
   `name` NVarChar(50) COLLATE utf8_general_ci, 
@@ -51,3 +64,33 @@ CREATE TABLE person (
   )
 ) ENGINE=InnoDB ROW_FORMAT=DYNAMIC DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE person COMMENT = '';
+/*******************************************************************************
+ * Unique Constraints
+ * ------------------
+ * Extracted at 2017-09-29 09:48:52
+ ******************************************************************************/
+
+ALTER TABLE member ADD CONSTRAINT UK9qv6yhjqm8iafto8qk452gx8h UNIQUE 
+    (email);
+
+/*******************************************************************************
+ * Foreign Key Constraints
+ * -----------------------
+ * Extracted at 2017-09-29 09:48:52
+ ******************************************************************************/
+
+ALTER TABLE media ADD CONSTRAINT fk_media_author FOREIGN KEY (authorID)
+  REFERENCES author (ID)
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
+ALTER TABLE loan ADD CONSTRAINT fk_loan_media FOREIGN KEY (mediaID)
+  REFERENCES media (ID)
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
+ALTER TABLE loan ADD CONSTRAINT fk_loan_person FOREIGN KEY (personID)
+  REFERENCES person (ID)
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
--- AlexandriaBiblioteket/TestData.sql
+++ AlexandriaBiblioteket/TestData.sql
@@ -2,30 +2,30 @@ use alexandria;
 
 
 insert into author values (1, 'Jamie Chan');
-insert into media values (100000, 'Java: Learn Java in One Day and Learn It Well.', '978-1539397830', 1, 1, null);
+insert into media values (100000, 'Java: Learn Java in One Day and Learn It Well.', '978-1539397830', 1, 1);
 
 insert into author values (2, 'Kathy Sierra');
-insert into media values (100001, 'Head First Java, 2nd Edition', '978-0596009205', 1, 2, null);
+insert into media values (100001, 'Head First Java, 2nd Edition', '978-0596009205', 1, 2);
 
 insert into author values (3, 'Herbert Schildt');
-insert into media values (100002, 'Java: The Complete Reference, Ninth Edition', '978-0071808552', 1, 3, null);
+insert into media values (100002, 'Java: The Complete Reference, Ninth Edition', '978-0071808552', 1, 3);
 
 insert into author values (4, 'Steve Tale');
-insert into media values (100003, 'Java: The Ultimate Beginners Guide to Java Programming', '978-1539591214', 1, 4, null);
-insert into media values (100004, 'JavaScript: The Ultimate Beginners Guide', '978-1539984634', 1, 4, null);
-insert into media values (100005, 'SQL: The Ultimate Beginners Guide - Learn SQL Today', '978-1533604569', 1, 4, null);
+insert into media values (100003, 'Java: The Ultimate Beginners Guide to Java Programming', '978-1539591214', 1, 4);
+insert into media values (100004, 'JavaScript: The Ultimate Beginners Guide', '978-1539984634', 1, 4);
+insert into media values (100005, 'SQL: The Ultimate Beginners Guide - Learn SQL Today', '978-1533604569', 1, 4);
 
 insert into author values (5, 'John Joseph Stockdale');
-insert into media values (100006, 'Island of Java (Periplus Classics Series)', '978-9628734238', 1, 5, null);
+insert into media values (100006, 'Island of Java (Periplus Classics Series)', '978-9628734238', 1, 5);
 
 insert into author values (6, 'John MacKinnon');
-insert into media values (100007, 'A Field Guide to the Birds of Borneo, Sumatra, Java, and Bali: The Greater Sunda Islands', '978-0198540342', 1, 6, null);
+insert into media values (100007, 'A Field Guide to the Birds of Borneo, Sumatra, Java, and Bali: The Greater Sunda Islands', '978-0198540342', 1, 6);
 
 insert into author values (7, 'Dian Pramesti');
-insert into media values (100008, 'Greater Than a Tourist – Java, Indonesia: 50 Travel Tips from a Local', '978-1521199466', 1, 7, null);
+insert into media values (100008, 'Greater Than a Tourist – Java, Indonesia: 50 Travel Tips from a Local', '978-1521199466', 1, 7);
 
 insert into author values (8, 'Jeffrey E. F. Friedl');
-insert into media values (100009, 'Mastering Regular Expressions', '978-0596528126', 1, 8, null);
+insert into media values (100009, 'Mastering Regular Expressions', '978-0596528126', 1, 8);
 
 
 INSERT INTO author (id,name)
@@ -47,20 +47,20 @@ VALUES (9, "Kathy Sierra"),
 		 (8,"Lisa Nilsson");
          
          
- INSERT INTO Media (id,name, ISBN, status,authorID,personID)
-VALUES (1000,"Head First Java 2nd Edition", 9780596009205,2, 9, 4),
-(1001, "Java: A Beginner's Guide Sixth Edition",9780071809252, 2, 10, 8),
-(1002, "Introduction to Java Programming: Brief Version, 10th Edition", 9780596007126, 2,11, 2),
-(1003, "Intro to Java Programming, Comprehensive Version (10th Edition)", 9780133761313,2, 12, 3),
-(1004, "Herregud & Co. Familjekalender 2018", 9789173875714,2, 13, 3); 
+ INSERT INTO Media (id,name, ISBN, status,authorID)
+VALUES (1000,"Head First Java 2nd Edition", 9780596009205,1, 9),
+(1001, "Java: A Beginner's Guide Sixth Edition",9780071809252, 1, 10),
+(1002, "Introduction to Java Programming: Brief Version, 10th Edition", 9780596007126, 1,11),
+(1003, "Intro to Java Programming, Comprehensive Version (10th Edition)", 9780133761313,2, 12),
+(1004, "Herregud & Co. Familjekalender 2018", 9789173875714,1, 13); 
 
 
 insert into loan (id, personid, mediaid, dateOut, dueBack, returned, fee)
-values (1, 4, 1000, "2017-01-01", "2017-03-27", "2017-09-01", "500 kr"),
-(2, 8, 1001, "2017-02-01", "2017-04-27", "2017-04-27", ""),
-(3, 2, 1002, "2017-05-10", "2017-08-27", "2017-09-27", "50 kr"),
-(4, 3, 1003, "2017-08-27", "2017-10-27", null, ""),
-(5, 3, 1004, "2017-08-27", "2017-10-27", "2017-09-30", "");
+values 	(1, 4, 1000, "2017-01-01", "2017-03-27", "2017-09-01", "500 kr"),
+		(2, 8, 1001, "2017-02-01", "2017-04-27", "2017-04-27", ""),
+		(3, 2, 1002, "2017-05-10", "2017-08-27", "2017-09-27", "50 kr"),
+		(4, 3, 1003, "2017-08-27", "2017-10-27", null, ""),
+		(5, 3, 1004, "2017-08-27", "2017-10-27", "2017-09-30", "");
 
          
       
